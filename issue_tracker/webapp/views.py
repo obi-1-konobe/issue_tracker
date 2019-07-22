b3b2f973505940649c80d70ba4bf103f18faa5de
Aldik <obi1konobe@gmail.com>
2019-07-22 17:13:51 +0600

    создано представление IndexView

--- issue_tracker/webapp/views.py
+++ issue_tracker/webapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from webapp.models import Issue
+
+
+class IndexView(ListView):
+    template_name = 'index.html'
+    context_object_name = 'issues'
+    paginate_by = 3
+    paginate_orphans = 1
+    
+    def get_queryset(self):
+        return Issue.objects.all().order_by('-created_at')
+    
