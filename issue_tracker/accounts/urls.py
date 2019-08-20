4a2c2de7264f3a8344fa7ed8e2153ee0ed2be58c
Aldik <obi1konobe@gmail.com>
2019-08-20 18:02:57 +0600

    Initial commit

--- issue_tracker/accounts/urls.py
+++ issue_tracker/accounts/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from .views import login_view, logout_view
+from django.contrib.auth.views import LoginView, LogoutView
+
+from .views import register_view
+
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login', login_view, name='login'),
-    path('logout', logout_view, name='logout'),
+    path('login', LoginView.as_view(), name='login'),
+    path('logout', LogoutView.as_view(), name='logout'),
+    path('create/', register_view, name='create')
 ]
--- issue_tracker/accounts/views.py
+++ issue_tracker/accounts/views.py
@@ -1,3 +1,4 @@
+from accounts.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 
 from django.shortcuts import render, redirect
@@ -8,15 +9,34 @@ def login_view(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
+        next_url = request.POST.get('next')
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+            if next_url != 'None':
+                return redirect(next_url)
             return redirect('webapp:index')
         else:
             context['has_error'] = True
+            context['next'] = next_url
+            context['user_name'] = username
+    else:
+        context = {'next': request.GET.get('next')}
     return render(request, 'login.html', context=context)
 
 
 def logout_view(request):
     logout(request)
     return redirect('webapp:index')
+
+def register_view(request, *args, **kwargs):
+    if request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('webapp:index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'create_user.html', context={'form': form})
+
